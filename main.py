8e2119d56d00bc96e8e3be81dd84e4ee0bd2d9c6
kuj1 <the.same.dentist@gmail.com>
2022-05-16 15:18:09 +0300

    added sorting by status, and writing emails in different files

--- main.py
+++ main.py
@@ -11,23 +11,52 @@ url = "https://public-sonjj.p.rapidapi.com/email-checker"
 
 file_input = str(input('Enter file name: '))
 
-with open(f'{file_input}', 'r') as doc:
-    for check_line in doc:
-        poor_email = check_line.split(';')[0]
 
-        r = requests.get(f'https://ychecker.com/app/key?email={poor_email}')
-        poor_key = r.text.split(',')[-1].split(':')[-1].replace('"}', '').replace('"', '')
+def main() -> None:
+    """
+    Checker email, with use api ychecker.com
+    :return: None
+    """
+    with open(f'{file_input}', 'r') as doc:
+        for check_line in doc:
+            poor_email = check_line.split(';')[0]
 
-        querystring = {
-            'email': f'{poor_email}',
-            'key': f'{poor_key}'
-        }
+            r = requests.get(f'https://ychecker.com/app/key?email={poor_email}')
+            poor_key = r.text.split(',')[-1].split(':')[-1].replace('"}', '').replace('"', '')
 
-        headers = {
-            "X-RapidAPI-Host": "public-sonjj.p.rapidapi.com",
-            "X-RapidAPI-Key": "f871a22852mshc3ccc49e34af1e8p126682jsn734696f1f081"
-        }
+            querystring = {
+                'email': f'{poor_email}',
+                'key': f'{poor_key}'
+            }
 
-        response = requests.request("GET", url, headers=headers, params=querystring)
+            headers = {
+                "X-RapidAPI-Host": "public-sonjj.p.rapidapi.com",
+                "X-RapidAPI-Key": "f871a22852mshc3ccc49e34af1e8p126682jsn734696f1f081"
+            }
 
-        print(f'{response.text}')
+            response = requests.request("GET", url, headers=headers, params=querystring)
+
+            print(response.text)
+            poor_state = response.text.split(',')[3].split(':')[-1].replace('"', '')
+            if poor_state == 'VerifyPhone':
+                with open(f'{dir_path}/verify_phone.txt', 'a') as file:
+                    file.write(f'{check_line}\n')
+            elif poor_state == 'NotExist':
+                with open(f'{dir_path}/not_exist.txt', 'a') as file:
+                    file.write(f'{check_line}\n')
+            elif poor_state == 'Disable':
+                with open(f'{dir_path}/disable.txt', 'a') as file:
+                    file.write(f'{check_line}\n')
+            elif poor_state == 'Disable|NotExist':
+                with open(f'{dir_path}/other.txt', 'a') as file:
+                    file.write(f'{check_line}\n')
+            elif poor_state == 'Ok':
+                with open(f'{dir_path}/ok.txt', 'a') as file:
+                    file.write(f'{check_line}\n')
+
+            with open(f'{dir_path}/log.txt', 'a') as log:
+                log.write(f'{response.text}\n')
+
+
+if __name__ == '__main__':
+    main()
